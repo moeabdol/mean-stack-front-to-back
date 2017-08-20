e4a2f8bb72066a8b1674794d1974e04026132e86
moeabdol <mohd.a.saed@gmail.com>
2017-08-20 22:34:37 +0300

    Change get to post in users routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const users = require("../controllers/users");
 
-router.get("/register", users.registerUser);
-router.get("/authenticate", users.authenticateUser);
+router.post("/register", users.registerUser);
+router.post("/authenticate", users.authenticateUser);
 router.get("/profile", users.getProfile);
 
 module.exports = router;
